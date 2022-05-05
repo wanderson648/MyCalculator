0ead4f96bb77abd818ad4747295b44584d12fc3f
wanderson648 <testepedroca@gmail.com>
2022-05-05 16:32:51 -0300

    UI Using LinearLayout part 1

---
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.mycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    fun onClick(view: View) {
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show();
+    }
+}
